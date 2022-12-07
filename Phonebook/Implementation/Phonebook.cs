6757ae5801dc12b8ff020634e80ba819e9fcd93f
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-12-07 14:26:51 +0300

    feat: complete deviding

---
+++ Phonebook/Implementation/Phonebook.cs
@@ -0,0 +1,135 @@
+﻿using Phonebook.Models;
+
+namespace Phonebook.Implementation
+{
+    /// <summary>
+    /// Главный класс
+    /// </summary>
+    public class PhonebookImplementation
+    {
+        private const string filepath = "phonebook.txt";
+
+        #region Events
+        public Action<string, ConsoleColor> OnDataChange;
+        #endregion
+
+        #region Singleton
+        /// <summary>
+        /// Синглтон
+        /// </summary>
+        private static PhonebookImplementation? instance;
+
+        private PhonebookImplementation()
+        {
+            if (!File.Exists(filepath))
+                File.Create(filepath);
+
+        }
+
+        public static PhonebookImplementation GetInstance()
+        {
+            if (instance == null)
+                instance = new PhonebookImplementation();
+            return instance;
+        }
+        #endregion
+
+        #region CRUD
+        /// <summary>
+        /// Ассинхронный метод записи нового контакта
+        /// </summary>
+        /// <param name="contact">На вход подаётся модель Contact</param>
+        /// <returns>True - контакт записан, False - контакт не записан</returns>
+        public async Task<bool> CreateContactAsync(Contact contact)
+        {
+            if (!File.Exists(filepath))
+                File.Create(filepath);
+            var contacts = await ReadContactAsync();
+
+            if (contacts.Contains(contact))
+                return false;
+
+            using (StreamWriter sw = new StreamWriter(filepath, true))
+            {
+                await sw.WriteLineAsync($"{contact}");
+                if (OnDataChange != null)
+                    OnDataChange(contact.name, ConsoleColor.Green);
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Ассинхронное получение контактов из файла
+        /// </summary>
+        /// <returns>Коллекция контактов</returns>
+        public async Task<List<Contact>> ReadContactAsync()
+        {
+            if (!File.Exists(filepath))
+                File.Create(filepath);
+            var contacts = new List<Contact>();
+            using (StreamReader reader = new StreamReader(filepath))
+            {
+                string? line;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    if (line == null || line == String.Empty)
+                        return contacts;
+                    var contactData = line.Split(":");
+                    contacts.Add(new Contact(
+                        contactData[0],
+                        contactData[1]
+                        ));
+                }
+            }
+            return contacts;
+        }
+
+        /// <summary>
+        /// Ассинхронный метод изменения контакта
+        /// </summary>
+        /// <param name="id">Идентификатор контакта</param>
+        /// <param name="contact">Заменяющие данные контакта</param>
+        /// <returns>Nothing</returns>
+        public async Task UpdateContact(int id, Contact contact)
+        {
+            var contacts = await ReadContactAsync();
+            contacts[id] = contact;
+            await RewriteFile(contacts);
+            if (OnDataChange != null)
+                OnDataChange(contacts[id].name, ConsoleColor.Yellow);
+        }
+
+        /// <summary>
+        /// Удаляет контакт по идентификатору
+        /// </summary>
+        /// <param name="contactId">Идентификатор контакта</param>
+        /// <returns>Another one (nothing)</returns>
+        public async Task DeleteContact(int contactId)
+        {
+            var contacts = await ReadContactAsync();
+            var contactToRemove = contacts[contactId];
+            contacts.Remove(contactToRemove);
+            await RewriteFile(contacts);
+            if (OnDataChange != null)
+                OnDataChange(contactToRemove.name, ConsoleColor.Red);
+        }
+        #endregion
+
+        /// <summary>
+        /// Вспомогательный метод перезаписи всех контактов
+        /// </summary>
+        /// <param name="contacts">Список контактов</param>
+        /// <returns>And another one (nothing)</returns>
+        private async Task RewriteFile(List<Contact> contacts)
+        {
+            using (StreamWriter sw = new StreamWriter(filepath, false))
+            {
+                foreach (var c in contacts)
+                {
+                    await sw.WriteLineAsync($"{c}");
+
+                }
+            }
+        }
+    }
+}
--- Phonebook/Program.cs
+++ Phonebook/Program.cs
@@ -1,2 +1,142 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Phonebook.Models;
+using Phonebook.Implementation;
+
+int command;
+
+var isRunning = true;
+var phonebook = PhonebookImplementation.GetInstance();
+phonebook.OnDataChange += DisplayDataChange;
+while (isRunning)
+{
+    Console.Clear();
+    Console.WriteLine("1. Добавить контакт");
+    Console.WriteLine("2. Просмотреть контакты");
+    Console.WriteLine("3. Изменить контакт");
+    Console.WriteLine("4. Удалить контакт");
+    Console.WriteLine("0. Выход");
+
+    if (!int.TryParse(Console.ReadLine(), out command))
+    {
+        Console.WriteLine("Ввод не корректен");
+        continue;
+    }
+
+    switch (command)
+    {
+        //Добавляем контакт
+        case 1:
+            {
+                Console.Clear();
+                Console.WriteLine("Добавить контакт");
+                Console.WriteLine("Введите имя");
+                var name = Console.ReadLine();
+                Console.WriteLine("Введите номер");
+                var phone = Console.ReadLine();
+                if (name == null || phone == null)
+                    Console.WriteLine("Похоже, что вы не ввели одно из полей");
+                else
+                {
+                    var answer = await phonebook.CreateContactAsync(new Contact(name, phone));
+                    if (!answer)
+                        Console.WriteLine("Не вышло =(\nПохоже такой контакт уже существует =/");
+                }
+                Console.WriteLine("Нажмите любую кнопку, чтобы выйти в меню");
+                Console.ReadKey();
+                break;
+            }
+        //Просматриваем контакты
+        case 2:
+            {
+                Console.Clear();
+                Console.WriteLine("Список контактов");
+                var contactList = await phonebook.ReadContactAsync();
+                if (contactList.Count > 0)
+                    for (var i = 0; i < contactList.Count; i++)
+                        Console.WriteLine($"{i + 1}. {contactList[i]}");
+                else
+                    Console.WriteLine("Здесь пусто D:");
+                Console.WriteLine("Нажмите любую кнопку, чтобы выйти в меню");
+                Console.ReadKey();
+                break;
+            }
+        //Изменяем контакт
+        case 3:
+            {
+                Console.Clear();
+                Console.WriteLine("Изменение контакта");
+                var contactList = await phonebook.ReadContactAsync();
+                if (contactList.Count > 0)
+                    for (var i = 0; i < contactList.Count; i++)
+                        Console.WriteLine($"{i + 1}. {contactList[i]}");
+                else
+                    Console.WriteLine("Здесь пусто D:");
+
+                Console.WriteLine("Выберите номер контакта");
+                int id;
+                if (!int.TryParse(Console.ReadLine(), out id))
+                {
+                    Console.WriteLine("Ввод не корректен");
+                    continue;
+                }
+
+                Console.WriteLine("Введите имя");
+                var name = Console.ReadLine();
+                Console.WriteLine("Введите номер");
+                var phone = Console.ReadLine();
+                if (name == null || phone == null)
+                    Console.WriteLine("Похоже, что вы не ввели одно из полей");
+                else
+                {
+                    await phonebook.UpdateContact(id - 1, new Contact(name, phone));
+                }
+                Console.WriteLine("Нажмите любую кнопку, чтобы выйти в меню");
+                Console.ReadKey();
+                break;
+            }
+        //Удолить
+        case 4:
+            {
+                Console.Clear();
+                Console.WriteLine("Удаление контакта");
+
+                var contactList = await phonebook.ReadContactAsync();
+                if (contactList.Count > 0)
+                    for (var i = 0; i < contactList.Count; i++)
+                        Console.WriteLine($"{i + 1}. {contactList[i]}");
+                else
+                    Console.WriteLine("Здесь пусто D:");
+
+                Console.WriteLine("Выберите номер контакта");
+                int id;
+                if (!int.TryParse(Console.ReadLine(), out id))
+                {
+                    Console.WriteLine("Ввод не корректен");
+                    continue;
+                }
+                else if (id > contactList.Count || id <= 0)
+                {
+                    Console.WriteLine("Ввод не корректен");
+                    continue;
+                }
+
+                await phonebook.DeleteContact((id - 1));
+                Console.WriteLine("Нажмите любую кнопку, чтобы выйти в меню");
+                Console.ReadKey();
+                break;
+            }
+        //Выйти в главное меню
+        case 0:
+            {
+
+                isRunning = false;
+                break;
+            }
+    }
+}
+
+void DisplayDataChange(string message, ConsoleColor consoleColor)
+{
+    Console.ForegroundColor = consoleColor;
+    Console.WriteLine(message);
+    Console.ResetColor();
+}
